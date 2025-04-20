4f23656cbfbd897e13c2417eb715320dacec081c
Caleb Robert Roy <yorbelac@gmail.com>
2025-04-20 14:31:38 -0500

    updated github pages url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 8080,
   },
   plugins: [react()],
-  base: '/auto-price-oracle/',
+  base: '/carpool/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
